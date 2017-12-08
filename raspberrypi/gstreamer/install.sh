0dde99df8ba17e756b283f2a97f8a656d7a6ad73
Somchai Somphadung <somchaisomph@gmail.com>
2017-12-08 09:29:53 +0700

    Update install.sh

--- raspberrypi/gstreamer/install.sh
+++ raspberrypi/gstreamer/install.sh
@@ -1,14 +1,12 @@
 sudo apt-get install \
 gir1.2-gst-plugins-bad-1.0 \
 gir1.2-gst-plugins-base-1.0 \
-gir1.2-gst-rtsp-server-1.0 \
 gir1.2-gstreamer-1.0 \
 gstreamer1.0-plugins-bad \
 gstreamer1.0-plugins-base \
 gstreamer1.0-plugins-good \
 gstreamer1.0-plugins-ugly \
 gstreamer1.0-pulseaudio \
-gstreamer1.0-rtsp \
 gstreamer1.0-tools \
 python3-gst-1.0 \
 python3-gi \
