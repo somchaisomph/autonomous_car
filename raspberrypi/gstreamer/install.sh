d939355c9715596011581441648392ea31b3f8c9
Somchai Somphadung <somchaisomph@gmail.com>
2017-12-08 09:29:13 +0700

    Create install.sh

---
+++ raspberrypi/gstreamer/install.sh
@@ -0,0 +1,19 @@
+sudo apt-get install \
+gir1.2-gst-plugins-bad-1.0 \
+gir1.2-gst-plugins-base-1.0 \
+gir1.2-gst-rtsp-server-1.0 \
+gir1.2-gstreamer-1.0 \
+gstreamer1.0-plugins-bad \
+gstreamer1.0-plugins-base \
+gstreamer1.0-plugins-good \
+gstreamer1.0-plugins-ugly \
+gstreamer1.0-pulseaudio \
+gstreamer1.0-rtsp \
+gstreamer1.0-tools \
+python3-gst-1.0 \
+python3-gi \
+gstreamer1.0-omx \
+gst-omx-listcomponents \
+gstreamer1.0-omx-rpi  \
+gstreamer1.0-omx-rpi-config \
+gstreamer1.0-omx-rpi-dbgsym 
